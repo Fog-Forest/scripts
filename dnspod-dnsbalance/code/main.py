89d4a185861ca172e2d4e79b2e054d12c2461e7e
Kinoko <admin@amogu.cn>
2021-12-24 16:35:16 +0800

    修复DNSPod DNS负载均衡脚本BUG

--- dnspod-dnsbalance/code/main.py
+++ dnspod-dnsbalance/code/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# @Author: Kinoko admin@amogu.cn
-# @Date  : 2021/10/22
-# @Desc  : 自动更新腾讯DNS记录，DNS负载均衡
-# @API   ： https://console.cloud.tencent.com/api/explorer
+# @Author: Kinoko <admin@amogu.cn>
+# @Date  : 2021/12/24
+# @Desc  : 自动更新腾讯DNS(DNSPod)负载均衡
+# @API   : https://console.cloud.tencent.com/api/explorer
 
 import json
+import random
+import ssl
 import time
 
 from tencentcloud.common import credential
@@ -13,13 +15,16 @@ from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.dnspod.v20210323 import dnspod_client, models
 
-# 您需要设置以下参数
-SecretId = "AKIDK********************14sSnt"
-SecretKey = "tXx84******************FPJxL"
+# 取消全局SSL证书验证
+ssl._create_default_https_context = ssl._create_unverified_context
 
-SubDomain = {"ddns.cn": ["ddns.01", "ddns.02", "ddns.03", "ddns.04", "ddns.05"]}  # 设置了 DDNS 的子域名
-# 设置DNS负载均衡的子域名，需要先在DNSPod手动创建记录，更新条数由手动创建的子域名个数决定且需要与上面记录个数相等（A记录，免费用户2条，付费用户10条）
-UpdateDomain = {"home.cn": "local"}
+# 您需要设置以下参数
+SecretId = "AKIDKVT**************j14sSnt"
+SecretKey = "tXx84*****************qFPJxL"
+
+DDNS = {"amogu.cn": ["ddns.01", "ddns.02", "ddns.03", "ddns.04", "ddns.05"]}  # DDNS子域名
+# DNS负载均衡的子域名，需要先在DNSPod手动创建记录，更新条数由手动创建的子域名个数决定且需要与上面DDNS子域名的个数相等（A记录，免费用户2条，付费用户10条）
+DNSBalance = {"amogu.cn": ["local"]}
 
 TTL = 600  # [1-86400]默认10分钟，免费版的范围是600-86400
 TIME = 300  # 检测间隔时间，单位秒
@@ -47,55 +52,88 @@ def get_record(domain, subdomain):
 
 
 # 更新子域名解析记录
-def update_record(domain, subdomain, value, recordid):
-    try:
-        cred = credential.Credential(SecretId, SecretKey)
-        httpProfile = HttpProfile()
-        httpProfile.endpoint = "dnspod.tencentcloudapi.com"
+def update_record(domain, subdomain, value, record_id):
+    cred = credential.Credential(SecretId, SecretKey)
+    httpProfile = HttpProfile()
+    httpProfile.endpoint = "dnspod.tencentcloudapi.com"
 
-        clientProfile = ClientProfile()
-        clientProfile.httpProfile = httpProfile
-        client = dnspod_client.DnspodClient(cred, "", clientProfile)
+    clientProfile = ClientProfile()
+    clientProfile.httpProfile = httpProfile
+    client = dnspod_client.DnspodClient(cred, "", clientProfile)
 
-        req = models.ModifyRecordRequest()
-        params = {
-            "Domain": domain,
-            "SubDomain": subdomain,
-            "RecordType": "A",
-            "RecordLine": "默认",
-            "Value": value,
-            "TTL": TTL,
-            "RecordId": recordid
-        }
-        req.from_json_string(json.dumps(params))
+    req = models.ModifyRecordRequest()
+    params = {
+        "Domain": domain,
+        "SubDomain": subdomain,
+        "RecordType": "A",
+        "RecordLine": "默认",
+        "Value": value,
+        "TTL": TTL,
+        "RecordId": record_id
+    }
+    req.from_json_string(json.dumps(params))
 
-        resp = client.ModifyRecord(req)
-        return resp.to_json_string()
-    except Exception as e:
-        print(e)
+    resp = client.ModifyRecord(req)
+    return resp.to_json_string()
+
+
+# 遍历记录值(IP)
+def research_record_value(domain, subdomain_list):
+    ip_list = []
+    # 判断是否是负载均衡域名
+    for subdomain in subdomain_list:
+        record_data = json.loads(get_record(domain, subdomain))["RecordList"]  # 获取记录内容
+        for record in record_data:
+            ip_list.append(record["Value"])
+    return ip_list
+
+
+# 遍历记录ID(DNS负载记录RecordID)
+def research_record_id(domain, subdomain):
+    id_list = []
+    record_data = json.loads(get_record(domain, subdomain))["RecordList"]
+    for record in record_data:
+        id_list.append(record["RecordId"])
+    return id_list
 
 
 # MAIN
 def main():
-    domain1 = list(SubDomain.keys())[0]  # DDNS子域名的根域名
-    domain2 = list(UpdateDomain.keys())[0]  # 需要设置DNS负载均衡子域名的根域名
-    subdomain1 = SubDomain[domain1]  # DDNS子域名的记录值（Type: List）
-    subdomain2 = UpdateDomain[domain2]  # 需要设置DNS负载均衡子域名的记录值
-    num = 0
+    ddns_domain = list(DDNS.keys())[0]  # DDNS的根域名
+    balance_domain = list(DNSBalance.keys())[0]  # DNS负载均衡的根域名
+    ddns_subdomain = DDNS[ddns_domain]  # DDNS子域名（Type: List）
+    balance_subdomain = DNSBalance[balance_domain]  # DNS负载均衡子域名
 
-    # 对比记录 IP
-    for record in subdomain1:
-        record_ip = json.loads(get_record(domain1, record))["RecordList"][0]["Value"]
-        update = json.loads(get_record(domain2, subdomain2))["RecordList"][num]
-        update_ip = update["Value"]
-        update_id = update["RecordId"]
-        # 两个记录相同无需更新
-        if record_ip == update_ip:
-            pass
+    ddns_ip_list = research_record_value(ddns_domain, ddns_subdomain)  # DDNS IP记录值
+    balance_ip_list = research_record_value(balance_domain, balance_subdomain)  # DNS负载均衡 IP记录值
+    balance_id_list = research_record_id(balance_domain, balance_subdomain[0])  # DNS负载均衡 RecordID
+    balance_num = len(ddns_subdomain)  # DNS负载均衡记录条数
+
+    """Debug
+    print(ddns_ip_list)
+    print(balance_ip_list)
+    print(balance_id_list)
+    """
+
+    # 开始处理
+    for ip in ddns_ip_list:
+        # 如果有IP不在DNS负载均衡记录值列表里则更新全部记录值
+        if ip not in balance_ip_list:
+            # 如果不一致则先修改DNS负载均衡子域名原先的记录值，防止IP重复无法更新记录
+            for num in range(balance_num):
+                # 生成随机IP地址
+                random_ip = "%d.%d.%d.%d" % (
+                    num + 1, random.randint(1, 254), random.randint(1, 254), random.randint(1, 254))
+                # 更新记录
+                update_record(balance_domain, balance_subdomain[0], random_ip, balance_id_list[num])
+            # 更新DNS负载均衡记录
+            for num in range(balance_num):
+                # 更新记录
+                update_record(balance_domain, balance_subdomain[0], ddns_ip_list[num], balance_id_list[num])
+                print("ddns_ip:", ddns_ip_list[num], "-> balance_ip:", balance_ip_list[num])
+            break
         else:
-            update_record(domain2, subdomain2, record_ip, update_id)
-            print("update ", update_ip, " to", record_ip, " success!")
-        num += 1
+            pass
 
 
 if __name__ == "__main__":
