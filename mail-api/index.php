ccb421260ae5ce48f66cef8c6dc717b01b93507a
Kinoko <admin@amogu.cn>
2022-04-02 00:40:46 +0800

    修改获取的参数列表

--- mail-api/index.php
+++ mail-api/index.php
@@ -6,23 +6,25 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
 //获取参数
-$title = $_GET['title'];
-$content = $_GET['content'];
+$address    = $_GET['address'];  //获取收件人邮箱
+$nickname   = $_GET['nickname']; //获取发件人昵称
+$title      = $_GET['title'];    //获取邮件标题
+$content    = $_GET['content'];  //获取邮件正文
 
 $mail = new PHPMailer();
-//SMTP配置
+//SMTP配置，请修改 Host\Username\Password\SMTPSecure\Port 为你的邮箱服务商信息
 $mail->SMTPDebug  = 0;                                      //E//Enable SMTP debugging, SMTP::DEBUG_OFF = off (for production use)
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'user@example.com';                     //SMTP username
 $mail->Password   = 'secret';                               //SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+$mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-//收件信息
-$mail->setFrom('from@example.com', 'Mailer');               //设置发件人信息
-$mail->addAddress('ellen@example.com');                     //设置收件人邮箱地址
+//收件信息，请修改 from@example.com 为你的发信邮箱
+$mail->setFrom('from@example.com', $nickname);              //设置发件人信息
+$mail->addAddress($address);                                //设置收件人邮箱地址
 //$mail->addAddress('joe@example.net', 'Joe User');         //收件人昵称可选
 //$mail->addReplyTo('info@example.com', 'Information');     //设置邮件的回复地址
 
@@ -32,11 +34,11 @@ $mail->addAddress('ellen@example.com');                     //设置收件人邮
 
 //正文
 $mail->isHTML(true);                                        //是否为HTML格式
-//$mail->Subject = 'Here is the subject';                                       // 邮件标题
-//$mail->Body    = 'This is the HTML message body <b>in bold!</b>';             // 邮件内容
+//$mail->Subject = 'Here is the subject';                                       // 邮件标题示例
+//$mail->Body    = 'This is the HTML message body <b>in bold!</b>';             // 邮件内容示例
 
 $mail->Subject = $title;
-//邮件模板
+//邮件模板示例
 $mail->Body    = <<<EOF
 <div
     style="border-radius:5px;font-size:13px;width:680px;font-family:微软雅黑,'Helvetica Neue',Arial,sans-serif;margin:10px auto 0px;border:1px solid #eee;max-width:100%;">
