42f69a7a8f17d27c8f8b4772ddbb8f7cecee4215
Kinoko <ye8980@gmail.com>
2023-02-04 05:02:24 +0800

    甲骨文保活脚本

--- oracle-lookbusy/app/docker-entrypoint.sh
+++ oracle-lookbusy/app/docker-entrypoint.sh
@@ -7,13 +7,13 @@ if [ $SPEEDTEST_INTERVAL ]; then
     if [ $SPEEDTEST_INTERVAL -eq 0 ]; then
         echo "Interval is 0, no task will be created"
     elif [ $SPEEDTEST_INTERVAL -le 59 ]; then
-        echo -e "*/${SPEEDTEST_INTERVAL}\t*\t*\t*\t*\tnohup speedtest >/app/speedtest.log 2>&1 &" | crontab
+        echo -e "*/${SPEEDTEST_INTERVAL}\t*\t*\t*\t*\tnohup bash /app/speedtest.sh >/app/speedtest.log 2>&1 &" | crontab
         echo "Scheduled task created successfully"
         service cron restart
     elif [[ $SPEEDTEST_INTERVAL -gt 59 && $SPEEDTEST_INTERVAL -lt 1440 ]]; then
         hour=$(($SPEEDTEST_INTERVAL / 60))
         minute=$(($SPEEDTEST_INTERVAL % 60))
-        echo -e "$minute\t*/$hour\t*\t*\t*\tnohup speedtest >/app/speedtest.log 2>&1 &" | crontab
+        echo -e "$minute\t*/$hour\t*\t*\t*\tnohup bash /app/speedtest.sh >/app/speedtest.log 2>&1 &" | crontab
         echo "Scheduled task created successfully"
         service cron restart
     else
